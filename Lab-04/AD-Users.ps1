59b904f80c08082c0c9a5c272ce800c52d0b505d
Tim Hinchey <36666848+timhincheydvsa@users.noreply.github.com>
2021-03-23 14:08:40 +0000

    Update AD-Users.ps1
    Re-wrote the foreach loop logic so the Set-ADUser and Move-ADObject commands are not repeated in script. This will be easier to maintain and modify, if you choose to accept it.
    Script has been tested successfully with 59 users.

--- Lab-04/AD-Users.ps1
+++ Lab-04/AD-Users.ps1
@@ -5,20 +5,40 @@ param(
 #Create Groups OU and 'All AdUsers' Group in Groups OU
 New-ADOrganizationalUnit Groups -ProtectedFromAccidentalDeletion $false
 $ADOU=Get-ADOrganizationalUnit -Filter "name -eq 'groups'"
-new-adgroup "All AdUsers" -GroupScope Global -Path $AdOU.DistinguishedName 
+new-adgroup "All AdUsers" -GroupScope Global -Path $AdOU.DistinguishedName
 #Create the rest of the OU's
 New-ADOrganizationalUnit 0-Zeros -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 1-Tens -ProtectedFromAccidentalDeletion $false
-New-ADOrganizationalUnit 2-Twenties -path "OU=1-Tens, DC=Aurian, DC=club" -ProtectedFromAccidentalDeletion $false
+New-ADOrganizationalUnit 2-Twenties -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 3-Thirties -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 4-Forties -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 5-Fifties -ProtectedFromAccidentalDeletion $false
 #Set the count for creating Users
 foreach ($i in 1..$Users) {
-    If ($i -lt 10) {
+    if ($i -lt 10) {
         $i = "0$i"
-    }else {
-        $i = $i
+        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '0-Zeros'"
+        $Department = "Zeros"
+    }
+    elseif ($i -ge 10 -and $i -lt 20){
+        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '1-Tens'"
+        $Department = "Tens"
+    }
+    elseif ($i -ge 20 -and $i -lt 30){
+        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '2-Twenties'"
+        $Department = "Twenties"
+    }
+    elseif ($i -ge 30 -and $i -lt 40){
+        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '3-Thirties'"
+        $Department = "Thirties"
+    }
+    elseif ($i -ge 40 -and $i -lt 50){
+        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '4-Forties'"
+        $Department = "Forties"
+    }
+    elseif ($i -ge 50 -and $i -lt 60){
+        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '5-Fifties'"
+        $Department = "Fifties"
     }
     New-ADUser -Name AdUser$i `
                -AccountPassword (ConvertTo-SecureString -AsPlainText "London2020*" -Force) `
@@ -29,36 +49,12 @@ foreach ($i in 1..$Users) {
                -PasswordNeverExpires $True `
                -SamAccountName AdUser$i `
                -UserPrincipalName "AdUser$i@$domainname"
-    if ($i -lt 10) {
-        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '0-Zeros'"
-        Set-ADUser -Identity "AdUser$i" -Department Zeros
-        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName 
-    }
-    elseif ($i -ge 10 -and $i -lt 20) { 
-        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '1-Tens'"
-        Set-ADUser -Identity "AdUser$i" -Department Tens
-        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 20 -and $i -lt 30) { 
-        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '2-Twenties'"
-        Set-ADUser -Identity "AdUser$i" -Department Twenties
-        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 30 -and $i -lt 40) { 
-        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '3-Thirties'"
-        Set-ADUser -Identity "AdUser$i" -Department Thirties
-        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 40 -and $i -lt 50) { 
-        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '4-Forties'"
-        Set-ADUser -Identity "AdUser$i" -Department Forties
-        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 50 -and $i -lt 60) { 
-        $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '5-Fifties'"
-        Set-ADUser -Identity "AdUser$i" -Department Fifties
-        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }
+    Set-ADUser -Identity "AdUser$i" -Department $Department
+    Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
     Add-ADGroupMember -Identity 'All AdUsers' -Members "AdUser$i"
-    Add-ADGroupMember -Identity 'Domain Admins' -Members "Domain Users"
 }
 
+Add-ADGroupMember -Identity 'Domain Admins' -Members "Domain Users"
 
 New-ADUser -Name OnPremAdmin `
                -AccountPassword (ConvertTo-SecureString -AsPlainText "London2020*" -Force) `
@@ -75,8 +71,6 @@ Add-ADGroupMember -Identity 'Domain Admins' -Members "OnPremadmin"
 Add-ADGroupMember -Identity 'Enterprise Admins' -Members "OnPremadmin"
 Add-ADGroupMember -Identity 'Group Policy Creator Owners' -Members "OnPremadmin"
 Add-ADGroupMember -Identity 'Schema Admins' -Members "OnPremadmin"
-
-
 
 dsa.msc
 
