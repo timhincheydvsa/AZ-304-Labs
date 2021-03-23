c236f72d8175a378ef17193e3b1d03de27f9e36a
Tim Hinchey <36666848+timhincheydvsa@users.noreply.github.com>
2021-03-23 13:44:48 +0000

    Update AD-Users.ps1
    Removed whitespace
    
    Made dsa.msc the final command to ensure focus does not remove from PS1 script before NETSH command starts
    
    Removed -Path attribute from New-ADOrganizationalUnit 2-Twenties command

--- Lab-04/AD-Users.ps1
+++ Lab-04/AD-Users.ps1
@@ -5,11 +5,11 @@ param(
 #Create Groups OU and 'All AdUsers' Group in Groups OU
 New-ADOrganizationalUnit Groups -ProtectedFromAccidentalDeletion $false
 $ADOU=Get-ADOrganizationalUnit -Filter "name -eq 'groups'"
-new-adgroup "All AdUsers" -GroupScope Global -Path $AdOU.DistinguishedName 
+new-adgroup "All AdUsers" -GroupScope Global -Path $AdOU.DistinguishedName
 #Create the rest of the OU's
 New-ADOrganizationalUnit 0-Zeros -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 1-Tens -ProtectedFromAccidentalDeletion $false
-New-ADOrganizationalUnit 2-Twenties -path "OU=1-Tens, DC=Aurian, DC=club" -ProtectedFromAccidentalDeletion $false
+New-ADOrganizationalUnit 2-Twenties -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 3-Thirties -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 4-Forties -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 5-Fifties -ProtectedFromAccidentalDeletion $false
@@ -32,25 +32,25 @@ foreach ($i in 1..$Users) {
     if ($i -lt 10) {
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '0-Zeros'"
         Set-ADUser -Identity "AdUser$i" -Department Zeros
-        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName 
+        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
     }
-    elseif ($i -ge 10 -and $i -lt 20) { 
+    elseif ($i -ge 10 -and $i -lt 20) {
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '1-Tens'"
         Set-ADUser -Identity "AdUser$i" -Department Tens
         Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 20 -and $i -lt 30) { 
+    }elseif ($i -ge 20 -and $i -lt 30) {
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '2-Twenties'"
         Set-ADUser -Identity "AdUser$i" -Department Twenties
         Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 30 -and $i -lt 40) { 
+    }elseif ($i -ge 30 -and $i -lt 40) {
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '3-Thirties'"
         Set-ADUser -Identity "AdUser$i" -Department Thirties
         Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 40 -and $i -lt 50) { 
+    }elseif ($i -ge 40 -and $i -lt 50) {
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '4-Forties'"
         Set-ADUser -Identity "AdUser$i" -Department Forties
         Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
-    }elseif ($i -ge 50 -and $i -lt 60) { 
+    }elseif ($i -ge 50 -and $i -lt 60) {
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '5-Fifties'"
         Set-ADUser -Identity "AdUser$i" -Department Fifties
         Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
@@ -58,7 +58,6 @@ foreach ($i in 1..$Users) {
     Add-ADGroupMember -Identity 'All AdUsers' -Members "AdUser$i"
     Add-ADGroupMember -Identity 'Domain Admins' -Members "Domain Users"
 }
-
 
 New-ADUser -Name OnPremAdmin `
                -AccountPassword (ConvertTo-SecureString -AsPlainText "London2020*" -Force) `
@@ -76,8 +75,6 @@ Add-ADGroupMember -Identity 'Enterprise Admins' -Members "OnPremadmin"
 Add-ADGroupMember -Identity 'Group Policy Creator Owners' -Members "OnPremadmin"
 Add-ADGroupMember -Identity 'Schema Admins' -Members "OnPremadmin"
 
-
+netsh advfirewall set allprofiles state off
 
 dsa.msc
-
-netsh advfirewall set allprofiles state off
